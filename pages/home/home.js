1b41275b406487e38febd8e51961a4eb812ae15f
unknown <unknown@example.com>
2023-01-05 13:47:18 +0800

    1.5(1)

--- pages/home/home.js
+++ pages/home/home.js
@@ -130,9 +130,17 @@ Component({
       console.log("yes", e)
       let index = e.currentTarget.dataset.index
       let url = this.data.functions[index].goto
-      wx.redirectTo({
-        url: url,
-      })
+      if (url == '/pages/checkM/checkM' && app.globalData.authority2 == 10) {
+        wx.showToast({
+          title: '您没有权限',
+          icon: 'error'
+        })
+      } else {
+        wx.redirectTo({
+          url: url,
+        })
+      }
+
     },
     onChange(e) {
       //tabbar
