e83a5f4ec21dda94a4ea3e0f30130ee93ffd5f6a
unknown <unknown@example.com>
2023-03-14 22:56:22 +0800

    3.14/绩效系统页面完成

--- pages/performance/performance.js
+++ pages/performance/performance.js
@@ -1,10 +1,14 @@
 // pages/performance/performance.js
+const PICKER_KEY = {
+  DEPART: 'depart',
+};
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    index1: '',
     performance: [{
       title: "完成第一篇稿件",
       style: false,
@@ -24,18 +28,143 @@ Page({
       title: "完成第二篇稿件",
       style: false,
       score: 20,
-      complete: false
+      complete: false,
+      people: [{
+        name: '乔晟豪',
+        check: true
+      }, {
+        name: '蔡毅洲',
+        check: false
+      }, {
+        name: '乔晟豪',
+        check: false
+      }]
     }, {
       title: "每月参加一次记者团培训/例会",
       style: false,
       score: 10,
-      complete: false
+      complete: false,
+      people: [{
+        name: '乔晟豪',
+        check: true
+      }, {
+        name: '蔡毅洲',
+        check: false
+      }, {
+        name: '乔晟豪',
+        check: false
+      }]
     }, {
       title: "完成多篇稿件（第三篇开始）",
       style: true,
       score: 10,
-      complete: false
+      complete: false,
+      people: [{
+        name: '乔晟豪',
+        check: true
+      }, {
+        name: '蔡毅洲',
+        check: false
+      }, {
+        name: '乔晟豪',
+        check: false
+      }]
     }],
+    PICKER_KEY,
+    [`${PICKER_KEY.DEPART}Visible`]: false,
+    pickerTitle: '',
+    [`${PICKER_KEY.DEPART}Value`]: [],
+    depart: [{
+      label: '人事部',
+      value: '人事部'
+    }, {
+      label: '记者团',
+      value: '记者团'
+    }, {
+      label: '技术部',
+      value: '技术部'
+    }, {
+      label: '记者团',
+      value: '记者团'
+    }, {
+      label: '运营部',
+      value: '运营部'
+    }, {
+      label: '新媒体部',
+      value: '新媒体部'
+    }]
+  },
+  showPeople(e) {
+    let index1 = e.currentTarget.dataset.index1;
+    this.setData({
+      index1: index1
+    })
+    console.log(e, index1);
+    let complete = 'performance[' + index1 + '].complete'
+    console.log(complete)
+    this.setData({
+      [complete]: !this.data.performance[index1].complete
+    })
+    console.log(this.data.performance[index1].complete)
+  },
+  checkPeople(e) {
+    let index2 = e.currentTarget.dataset.index2;
+    let index1 = this.data.index1
+    let check = 'performance[' + index1 + '].people.[' + index2 + '].check'
+    this.setData({
+      [check]: !this.data.performance[index1].people[index2].check
+    })
+  },
+  joinArray(array) {
+    return array.join(' ');
+  },
+  onClickPicker(e) {
+    const {
+      key
+    } = e?.currentTarget?.dataset;
+    //console.log('picker pick:', place1);
+    this.setData({
+      [`${key}Visible`]: true,
+    });
+  },
+  onColumnChange(e) {
+    //console.log('picker pick:', e);
+  },
+  onPickerChange(e) {
+    const {
+      key
+    } = e?.currentTarget?.dataset;
+    //console.log('picker change:', );
+    this.setData({
+      [`${key}Visible`]: false,
+      [`${key}Value`]: e.detail.value,
+      [`${key}CurrentValue`]: this.joinArray(e.detail.label),
+    });
+    if (e.detail.value.length == 4) {
+      this.setData({
+        list1: e.detail.value
+      })
+    }
+    if (e.detail.label[0] == e.detail.value[0] + '时') {
+      this.setData({
+        list2: e.detail.value
+      })
+    }
+    if (e.detail.label[0] == e.detail.value[0] + '文') {
+      this.setData({
+        list3: e.detail.value
+      })
+    }
+  },
+  onPickerCancel(e) {
+    const {
+      key
+    } = e?.currentTarget?.dataset;
+    console.log(e, '取消');
+    console.log('picker1 cancel:');
+    this.setData({
+      [`${key}Visible`]: false,
+    });
   },
 
   /**
