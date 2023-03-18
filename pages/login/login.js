d02de17352883cb887d4a7a0f9e7f4f6eea99cc7
cyz <c1059124823@163.com>
2023-03-18 14:17:40 +0800

    3.18.2

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,186 +1,241 @@
-  // pages/login/login.js
-  const app = getApp()
-  Page({
-    content(e) {
-      this.setData({
-        userid: e.detail.value
+// pages/login/login.js
+const app = getApp()
+Page({
+  content(e) {
+    this.setData({
+      userid: e.detail.value
+    })
+  },
+  password(e) {
+    this.setData({
+      password: e.detail.value
+    })
+  },
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    open: true
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {},
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  },
+  password_eye() {
+    let open = !this.data.open
+    this.setData({
+      open: open
+    })
+  },
+  Login1() {
+    if (this.data.password === '') {
+      wx.showToast({
+        title: '请输入密码！',
+        icon: 'error'
       })
-    },
-    password(e) {
-      this.setData({
-        password: e.detail.value
+    }
+    if (this.data.userid === '') {
+      wx.showToast({
+        title: '请输入账户！',
+        icon: 'error'
       })
-    },
+    }
+  },
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-      open: true
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    open: true
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {},
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {},
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
 
-    },
+  },
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
 
-    },
+  },
 
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
 
-    },
+  },
 
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
 
-    },
-    password_eye() {
-      let open = !this.data.open
-      this.setData({
-        open: open
+  },
+  password_eye() {
+    let open = !this.data.open
+    this.setData({
+      open: open
+    })
+  },
+  Login1() {
+    if (this.data.password === '') {
+      wx.showToast({
+        title: '请输入密码！',
+        icon: 'error'
       })
-    },
-    Login1() {
-      if (this.data.password === '') {
-        wx.showToast({
-          title: '请输入密码！',
-          icon: 'error'
-        })
-      }
-      if (this.data.userid === '') {
-        wx.showToast({
-          title: '请输入账户！',
-          icon: 'error'
-        })
-      } else {
-        wx.request({
-          data: {
-            data: {
-              password: '123456', //this.data.password
-              userid: 'U202116242'//this.data.userid
-            },
-            method: "signUp"
-          },
-          url: 'http://1.15.118.125:8081/NIC/login',
-          success: (res) => {
-            console.log(res.data);
-            if (res.data.code === 102) {
-              app.globalData.hasLogin = true
-              app.globalData.authority1 = res.data.data.authority1
-              app.globalData.authority2 = res.data.data.authority2
-              app.globalData.authority3 = res.data.data.authority3
-              app.globalData.username = res.data.data.username
-              app.globalData.userid = res.data.data.userid
-              wx.showToast({
-                  title: '登录成功',
-                }),
-                app.sleep(1200)
-              wx.getUserProfile({
-                desc: '是否授权？',
-                success: (res) => {
-                  console.log(res)
-                  let datalist1 = res
-                  wx.login({
-                    success: (res) => {
-                      console.log(res)
-                      let datalist2 = res
-                      wx.request({
-                        url: '',
-                        data: {
-                          'encryptedData': datalist1.encryptedData,
-                          'iv': datalist1.iv,
-                          'signature': datalist1.signature,
-                          'rawData': datalist1.rawData,
-                          'code': datalist2.code,
-                          'userid':app.globalData.userid
-                        },
-                        success: (res) => {
-                          console.log(res)
-                        }
-                      })
-                    }
-                  })
-                }
-              })
-              wx.redirectTo({
-                url: '/pages/home/home',
-              })
-            }
-            if (res.data.code === 99) {
-              wx.showToast({
-                title: '查无此用户',
-                icon: 'error'
-              })
-            }
-            if (res.data.code === 101) {
-              wx.showToast({
-                title: '密码错误',
-                icon: 'error'
-              })
-            }
-            if (res.data.code === 103) {
-              wx.showToast({
-                title: 'Error',
-                icon: 'error'
-              })
-            }
-          }
-        })
-      }
-    },
-    Login2() {
+    }
+    if (this.data.userid === '') {
+      wx.showToast({
+        title: '请输入账户！',
+        icon: 'error'
+      })
+    } else {
       wx.request({
         data: {
-          method: "tourist"
+          data: {
+            password: '123456', //this.data.password
+            userid: 'U202116242' //this.data.userid
+          },
+          method: "signUp"
         },
         url: 'http://1.15.118.125:8081/NIC/login',
         success: (res) => {
+          console.log(res.data);
           if (res.data.code === 102) {
-            app.globalData.hasLogin = true;
-            wx.redirectTo({
-                url: '/pages/home/home',
-              }),
-              wx.showToast({
+            app.globalData.hasLogin = true
+            app.globalData.authority1 = res.data.data.authority1
+            app.globalData.authority2 = res.data.data.authority2
+            app.globalData.authority3 = res.data.data.authority3
+            app.globalData.username = res.data.data.username
+            app.globalData.userid = res.data.data.userid
+            wx.showToast({
                 title: '登录成功',
-              })
+              }),
+              app.sleep(1200)
+            wx.getUserProfile({
+              desc: '是否授权？',
+              success: (res) => {
+                console.log(res)
+                let datalist1 = res
+                wx.login({
+                  success: (res) => {
+                    console.log(res)
+                    let datalist2 = res
+                    wx.request({
+                      url: '',
+                      data: {
+                        'encryptedData': datalist1.encryptedData,
+                        'iv': datalist1.iv,
+                        'signature': datalist1.signature,
+                        'rawData': datalist1.rawData,
+                        'code': datalist2.code,
+                        'userid': app.globalData.userid
+                      },
+                      success: (res) => {
+                        console.log(res)
+                      }
+                    })
+                  }
+                })
+              }
+            })
+            wx.redirectTo({
+              url: '/pages/home/home',
+            })
+          }
+          if (res.data.code === 99) {
+            wx.showToast({
+              title: '查无此用户',
+              icon: 'error'
+            })
+          }
+          if (res.data.code === 101) {
+            wx.showToast({
+              title: '密码错误',
+              icon: 'error'
+            })
           }
           if (res.data.code === 103) {
             wx.showToast({
@@ -190,5 +245,82 @@
           }
         }
       })
-    },
-  })
+    }
+  },
+  Login2() {
+    wx.request({
+      data: {
+        data: {
+          password: '123456', //this.data.password
+          userid: this.data.userid
+        },
+        method: "signUp"
+      },
+      url: 'http://1.15.118.125:8081/NIC/login',
+      success: (res) => {
+        console.log(res.data);
+        if (res.data.code === 102) {
+          app.globalData.hasLogin = true
+          app.globalData.authority1 = res.data.data.authority1
+          app.globalData.authority2 = res.data.data.authority2
+          app.globalData.authority3 = res.data.data.authority3
+          app.globalData.username = res.data.data.username
+          app.globalData.userid = res.data.data.userid
+          wx.showToast({
+              title: '登录成功',
+            }),
+            app.sleep(1200)
+          wx.getUserProfile({
+            desc: '是否授权？',
+            success: (res) => {
+              console.log(res)
+              let datalist1 = res
+              wx.login({
+                success: (res) => {
+                  console.log(res)
+                  let datalist2 = res
+                  wx.request({
+                    url: '',
+                    data: {
+                      'encryptedData': datalist1.encryptedData,
+                      'iv': datalist1.iv,
+                      'signature': datalist1.signature,
+                      'rawData': datalist1.rawData,
+                      'code': datalist2.code,
+                      'userid': app.globalData.userid
+                    },
+                    success: (res) => {
+                      console.log(res)
+                    }
+                  })
+                }
+              })
+            }
+          })
+          wx.redirectTo({
+            url: '/pages/home/home',
+          })
+        }
+        if (res.data.code === 99) {
+          wx.showToast({
+            title: '查无此用户',
+            icon: 'error'
+          })
+        }
+        if (res.data.code === 101) {
+          wx.showToast({
+            title: '密码错误',
+            icon: 'error'
+          })
+        }
+        if (res.data.code === 103) {
+          wx.showToast({
+            title: 'Error',
+            icon: 'error'
+          })
+        }
+      }
+    })
+  }
+}
+)
---
+++ pages/managment/managment.js
@@ -0,0 +1,134 @@
+// pages/managment/managment.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    people: [{
+      name: '组长会',
+      check: true
+    }, {
+      name: 'NIC例会',
+      check: false
+    }, {
+      name: '发放薪酬',
+      check: false
+    }, {
+      name: '组织团建',
+      check: false
+    }],
+    info: [{
+      column: "1/3",
+      row: "3/5",
+      color: "black",
+      btext: "3",
+      stext: "待审核任务",
+      class: 'maxnum',
+      f: "changeName"
+    }, {
+      column: "3/5",
+      row: "3/5",
+      color: "#574c45",
+      btext: "4",
+      stext: " 进行中任务",
+      class: 'maxnum',
+      f: ""
+    },
+    //  {
+    //   column: "1/5",
+    //   row: "1/3",
+    //   color: "#0e6e8c",
+    //   btext: "待审核稿件：3",
+    //   stext: "任务进度",
+    //   class: '-max',
+    //   f: "changeName"
+    // },
+    {
+      column: "1/2",
+      row: "5/6",
+      color: "#d56937",
+      btext: "50",
+      stext: "人员系统",
+      f: ""
+    }, {
+      column: "2/3",
+      row: "5/6",
+      color: "#8fb3a5",
+      btext: "30",
+      stext: "本月任务",
+      f: ""
+    }, {
+      column: "3/5",
+      row: "5/7",
+      color: "#4a2028",
+      btext: "888",
+      stext: "组织总绩效",
+      class: "maxnum",
+      f: ""
+    }],
+  },
+  checkitems(e) {
+    console.log(e);
+    let index1 = e.currentTarget.dataset.index1;
+    let check = 'people.[' + index1 + '].check'
+    this.setData({
+      [check]: !this.data.people[index1].check
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
